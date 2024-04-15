bf1fc07a69487bc4add3ad3a758a927129bf06c6
Kaheel1 <158682777+Kaheel1@users.noreply.github.com>
2024-04-14 17:15:47 -0700

    Add files via upload

--- Code.js
+++ Code.js
@@ -122,3 +122,61 @@ function getRandomCoordinate(){
     // This function will return random coordinates back to the code that called it
     return Math.floor(Math.random() * 1000);
 }
+
+// *******************
+// Heare are the 2 functions that run the palindrome checking
+function palindromesFun(){
+    // console.log(palindromesTest("radar"));
+    // console.log(palindromesTest("Fun stuff"));
+
+    // Flag variable to test if the user wants to continue
+    var bContinue = true;
+    do{
+        // Prompt the user to enter a word
+        var userInput = prompt("Enter a word to see if it a palindrome: ");
+        // Check to see if the user's word is a palindrome
+        var bIsPalindrome = palindromesTest(userInput);
+
+        //  Create a message for the user
+        var message = "";
+        // If the word is a palindrome, let the user know
+        if(bIsPalindrome){
+            // Tell the user that their word is a palindrome
+            message = userInput + " is a palindrome!";
+        }
+        // If the word is Not a palindrome, let the user know
+        else{
+            // Tell the user that their word is Not a palindrome
+            message = userInput + " is not a palindrome!";
+        }
+        // Display message to the user
+        alert(message);
+        
+        // Prompt the user to see if they want to continue
+        var answer = prompt("Do you want to continue? (y/n)");
+        // Check the answer , if n, exit the loop
+        if(answer == "n"){
+            // Set our boolean loop monitor to false so that the loop will stop running
+            bContinue = false;
+        }
+    }
+    while(bContinue); // While bContinue is true, the loop will keep running; if bContinue is false, the loop will not run anymore
+}
+// This function will test a string to see if it is the same forwards and backwards
+function palindromesTest(strToTest){
+    // Remove the white space from the string and make it lowercase
+    var strCleaned = strToTest.replace(/\s/g, "").toLowerCase();
+    console.log("strCleaned=" + strCleaned);
+
+    // Put the string into an array so we can easily reverse it
+    var strArray = strCleaned.split("");
+    // Now lets reverse the order of the letters in the array
+    var strReversedArray = strArray.reverse();
+    // Return the array back into a string
+    var strRev =strReversedArray.join("");
+    console.log("strRev=" + strRev);
+
+    // Test to see if the 2 strings are the same, both forwards and backwards
+    // If the words are the same, the function will return true; if not, the function will return false
+    return strRev == strCleaned;
+}
